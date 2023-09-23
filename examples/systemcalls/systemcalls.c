9e5c114853a148c8cd0cd0bfb87a9f9a23396786
nikhil <nikhil@Ubuntu22.04.myguest.virtualbox.org>
2023-09-23 11:01:57 +0530

    Implement Assignment 3 Part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,8 @@
 #include "systemcalls.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +20,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int ret = system(cmd);
+
+    if(ret != 0) 
+        return false;
 
     return true;
 }
@@ -58,6 +66,25 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int pid = fork();
+
+    if(pid == -1) 
+        return false;
+    else if(pid == 0) {
+        int err = execv(command[0], command);
+        printf("SDF: %d\n", err);
+        if(err < 0) {
+            exit(-1);
+        }
+
+        exit(0);
+    }
+
+    int status;
+    if(waitpid(pid, &status, 0) == -1)
+        return false;
+    else if(WIFEXITED(status) && WEXITSTATUS(status) != 0)
+        return false;
 
     va_end(args);
 
@@ -92,6 +119,32 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int pid = fork();
+
+    if(pid == -1) 
+        return false;
+    else if(pid == 0) {
+        int fid = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+        if(fid < 0) 
+            return false;
+
+        if(dup2(fid, 1) < 0) 
+            return false;
+        close(fid);
+
+        int err = execv(command[0], command);
+        if(err == -1) 
+            exit(-1);
+
+        exit(0);
+    }
+
+    int status;
+    if(waitpid(pid, &status, 0) == -1)
+        return false;
+    else if(WIFEXITED(status) && WEXITSTATUS(status) != 0)
+        return false;
+
 
     va_end(args);
 
